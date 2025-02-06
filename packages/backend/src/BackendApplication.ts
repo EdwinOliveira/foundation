76a78a9845e962f8f4d31aaf9df7b3b160a283d0
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-02-06 19:48:16 +0000

    WIP: Removed Socket dependency

--- packages/backend/src/BackendApplication.ts
+++ packages/backend/src/BackendApplication.ts
@@ -1,28 +1,19 @@
-import { createServer } from "node:http";
 import Express from "express";
-import { Server } from "socket.io";
 
 import "dotenv/config";
 
 const BackendApplication = () => {
 	const httpAddress = Number.parseInt(process.env.HTTP_ADDRESS ?? "8000");
 	const httpApplication = Express();
-	const httpServer = createServer(httpApplication);
-	const httpSocket = new Server(httpServer);
-
-	const createSocket = () => {
-		httpSocket.on("connection", () => {});
-	};
 
 	const createListner = () =>
 		httpApplication.listen(httpAddress, () =>
 			console.log(`Server initialized on PORT:${httpAddress}`),
 		);
 
-	return { createSocket, createListner };
+	return { createListner };
 };
 
-const { createSocket, createListner } = BackendApplication();
+const { createListner } = BackendApplication();
 
-createSocket();
 createListner();
