3c33de2f4c3354799619d15abf2218f3de623a1e
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-02-06 19:20:01 +0000

    WIP: Created Monorepo. Added Monorepo dependencies. Created Backend package. Added Backend package required dependencies

---
+++ packages/backend/src/BackendApplication.ts
@@ -0,0 +1,28 @@
+import Express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+import "dotenv/config";
+
+const BackendApplication = () => {
+	const httpAddress = Number.parseInt(process.env.HTTP_ADDRESS ?? "8000");
+	const httpApplication = Express();
+	const httpServer = createServer(httpApplication);
+	const httpSocket = new Server(httpServer);
+
+	const createSocket = () => {
+		httpSocket.on("connection", () => {});
+	};
+
+	const createListner = () =>
+		httpApplication.listen(httpAddress, () =>
+			console.log(`Server initialized on PORT:${httpAddress}`),
+		);
+
+	return { createSocket, createListner };
+};
+
+const { createSocket, createListner } = BackendApplication();
+
+createSocket();
+createListner();
