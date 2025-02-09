1686c9ee1a7bf05d5dc3fc4c5df68dac6897d2f5
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-02-09 22:22:14 +0000

    WIP: Updated uppercase pipes

--- packages/frontend/src/app/components/tracker/tracker.component.ts
+++ packages/frontend/src/app/components/tracker/tracker.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from "@angular/core";
 import { TypographyComponent } from "../typography/typography.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
 	selector: "app-tracker",
-	imports: [TypographyComponent],
+	imports: [TypographyComponent, CommonModule],
 	templateUrl: "./tracker.component.html",
 	styleUrl: "./tracker.component.scss",
 })
