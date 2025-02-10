ee267085a98e57dc0e6968b2769b34ff1a867f02
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2025-02-10 09:38:19 +0000

    WIP: Updated tests

--- packages/frontend/src/app/app.component.spec.ts
+++ packages/frontend/src/app/app.component.spec.ts
@@ -3,13 +3,13 @@ import { AppComponent } from "./app.component";
 import { ErrorHandlerService } from "./services/error-handler.service";
 import { StreamService } from "./services/stream.service";
 import { By } from "@angular/platform-browser";
-import { TrackerComponent } from "./components/tracker/tracker.component";
-import { GridComponent } from "./components/grid/grid.component";
 import { ButtonComponent } from "./components/button/button.component";
 import { CharacterSelectorComponent } from "./components/character-selector/character-selector.component";
+import { of } from "rxjs";
 
 describe("AppComponent", () => {
 	let fixture: ComponentFixture<AppComponent>;
+	const spies = new Array<jasmine.Spy>();
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
@@ -20,41 +20,31 @@ describe("AppComponent", () => {
 		fixture = TestBed.createComponent(AppComponent);
 	});
 
-	describe("ErrorElement", () => {
-		let service: ErrorHandlerService;
-		let errorElement: HTMLElement;
-
-		beforeEach(() => {
-			service = TestBed.inject(ErrorHandlerService);
-			service.error$.next("Hello Dummy Error");
-			errorElement = fixture.debugElement.nativeElement.querySelector("#error");
-			fixture.detectChanges();
-		});
-
-		afterEach(() => {
-			service.error$.unsubscribe();
-		});
-
-		it("@outputs element that notify there was an error triggered somewhere within the app", () => {
-			expect(errorElement.innerText).toBe("Hello Dummy Error");
-		});
+	afterEach(() => {
+		for (const spy of spies) {
+			spy.calls.reset();
+		}
 	});
 
 	describe("ActionsElement", () => {
+		let service: StreamService;
+
+		beforeEach(() => {
+			service = TestBed.inject(StreamService);
+		});
+
 		describe("CharacterSelectorComponent", () => {
 			let characterSelectorComponent: CharacterSelectorComponent;
-			let service: StreamService;
 
 			beforeEach(() => {
-				service = TestBed.inject(StreamService);
-
 				characterSelectorComponent = fixture.debugElement.query(
 					By.directive(CharacterSelectorComponent),
 				).componentInstance;
 			});
 
 			it("@subscribes updateStreamPriorityCharacter$ when the onCharacterInput event is triggered", () => {
-				spyOn(service, "updateStreamPriorityCharacter$");
+				const spy = spyOn(service, "updateStreamPriorityCharacter$");
+				spies.push(spy);
 				characterSelectorComponent.onCharacterInput.emit("A");
 				fixture.detectChanges();
 				expect(service.updateStreamPriorityCharacter$).toHaveBeenCalledWith(
@@ -66,11 +56,8 @@ describe("AppComponent", () => {
 		describe("ButtonComponent", () => {
 			let buttonComponent: ButtonComponent;
 			let buttonElement: HTMLElement;
-			let service: StreamService;
 
 			beforeEach(() => {
-				service = TestBed.inject(StreamService);
-
 				buttonComponent = fixture.debugElement.query(
 					By.directive(ButtonComponent),
 				).componentInstance;
@@ -85,7 +72,9 @@ describe("AppComponent", () => {
 			});
 
 			it("@subscribes createStream$ when the onGenerateStream event is triggered", () => {
-				spyOn(service, "createStream$");
+				const spy = spyOn(service, "createStream$");
+				spies.push(spy);
+
 				buttonComponent.onGenerateStream.emit();
 				fixture.detectChanges();
 				expect(service.createStream$).toHaveBeenCalled();
@@ -94,48 +83,67 @@ describe("AppComponent", () => {
 	});
 
 	describe("GridComponent", () => {
-		let gridComponent: GridComponent;
+		let buttonComponent: ButtonComponent;
+		let service: StreamService;
 
 		beforeEach(() => {
-			gridComponent = fixture.debugElement.query(
-				By.directive(GridComponent),
+			service = TestBed.inject(StreamService);
+
+			buttonComponent = fixture.debugElement.query(
+				By.directive(ButtonComponent),
 			).componentInstance;
 
-			gridComponent.characters = ["A", "B"];
+			const spy = spyOn(service, "createStream$").and.returnValue(
+				of({ characters: { "00": "A", "01": "B" }, code: "" }),
+			);
+
+			spies.push(spy);
+
+			buttonComponent.onGenerateStream.emit();
 			fixture.detectChanges();
 		});
 
 		it("@contains grid items with predefined inputs", () => {
 			expect(
 				fixture.debugElement.nativeElement.querySelector("#grid--item__0")
-					.innerText,
+					.textContent,
 			).toEqual("A");
 			expect(
 				fixture.debugElement.nativeElement.querySelector("#grid--item__1")
-					.innerText,
+					.textContent,
 			).toEqual("B");
 		});
 	});
 
 	describe("TrackerComponent", () => {
-		let trackerComponent: TrackerComponent;
 		let trackerCodeLiveElement: HTMLElement;
+		let service: StreamService;
+		let buttonComponent: ButtonComponent;
 
 		beforeEach(() => {
-			trackerComponent = fixture.debugElement.query(
-				By.directive(TrackerComponent),
-			).componentInstance;
+			service = TestBed.inject(StreamService);
 
 			trackerCodeLiveElement = fixture.debugElement.nativeElement.querySelector(
 				"#tracker--code__live",
 			);
 
-			trackerComponent.code = "00";
+			buttonComponent = fixture.debugElement.query(
+				By.directive(ButtonComponent),
+			).componentInstance;
+
+			const spy = spyOn(service, "createStream$").and.returnValue(
+				of({ characters: {}, code: "01" }),
+			);
+
+			spies.push(spy);
+
+			buttonComponent.onGenerateStream.emit();
+
 			fixture.detectChanges();
 		});
 
 		it("@contains predefined inputs", () => {
-			expect(trackerCodeLiveElement.innerText).toEqual("00");
+			expect(trackerCodeLiveElement.innerText).toEqual("01");
 		});
 	});
 });
--- packages/frontend/src/app/components/grid/grid.component.spec.ts
+++ packages/frontend/src/app/components/grid/grid.component.spec.ts
@@ -1,23 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { type ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GridComponent } from './grid.component';
+import { GridComponent } from "./grid.component";
 
-describe('GridComponent', () => {
-  let component: GridComponent;
-  let fixture: ComponentFixture<GridComponent>;
+describe("GridComponent", () => {
+	let component: GridComponent;
+	let fixture: ComponentFixture<GridComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GridComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [GridComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(GridComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(GridComponent);
+		component = fixture.componentInstance;
+		component.characters = ["A", "B"];
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("@contains grid items with predefined inputs", () => {
+		expect(
+			fixture.debugElement.nativeElement.querySelector("#grid--item__0")
+				.innerText,
+		).toEqual("A");
+		expect(
+			fixture.debugElement.nativeElement.querySelector("#grid--item__1")
+				.innerText,
+		).toEqual("B");
+	});
 });
